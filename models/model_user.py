220c07cfea28580d146a03a3cbbdea17a5730cc6
Yash Ballani <yash.ballani94@gmail.com>
2018-09-13 19:27:13 -0700

    added: error handling for db connections and data fetching/commiting

--- models/model_user.py
+++ models/model_user.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, String, Text, BigInteger
+from sqlalchemy.exc import OperationalError
 from sqlalchemy.ext.declarative import declarative_base
 
 from manager.manager_database import DatabaseManager
@@ -6,7 +7,11 @@ from manager.manager_database import DatabaseManager
 DBManager = DatabaseManager()
 Base = declarative_base()
 Base.metadata.bind = DBManager.fetch_engine()
-Base.metadata.create_all(DBManager.fetch_engine())
+try:
+    Base.metadata.create_all()
+except OperationalError:
+    print("Unable to connect to database. Please check database settings in settings.py")
+    exit()
 
 
 class User(Base):
